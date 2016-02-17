7fe7111f7c1080fc791f476de63e4f2889a77a9a
thoddi <thorvardur13@ru.is>
2016-02-17 10:06:10 +0000

    .

--- Client/app.js
+++ Client/app.js
@@ -1,13 +1,13 @@
-var ChatClient = angular.module('ChatClient', ['ngRoute']);
+"use strict";
 
-ChatClient.config(["$routeProvider", function ($routeProvider) {
+angular.module('chatapp', ['ngRoute'])
+.config($routeProvider) {
     $routeProvider
         .when('/', { templateUrl: 'login/login.html', controller: 'LoginController' })
         .when('/signup', { templateUrl: 'signup/signup.html', controller: 'SignUpController' })
         /*.when('/:user/', { templateUrl: 'rooms/rooms.html', controller: 'RoomsController' })
-        .when('/room/:user/:room/', { templateUrl: 'Views/room.html', controller: 'RoomController' })
+        .when('/room/:user/:room/', { templateUrl: 'Views/room.html', controller: 'RoomController' })*/
         .otherwise({
-            redirectTo: '/login'
-        });*/
-	}]
-);
+            redirectTo: '/'
+        });
+}
