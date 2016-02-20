e00e7156e81afc09040dbbc50fdc5005136deb8c
thoddi <thorvardur13@ru.is>
2016-02-20 23:10:34 +0000

    Private chat works like a dream. Ohh the glory!!

--- Client/src/Rooms/RoomsController.js
+++ Client/src/Rooms/RoomsController.js
@@ -6,7 +6,9 @@ angular.module('chatApp').controller('RoomsController', function($scope, socket,
     $scope.errorMessage = "";
     $scope.roomName = "";
     $scope.msgs = [];
-    var sendTo = document.getElementById("sendto").selected;
+    $scope.senders = [];
+    
+    //var sendTo = document.getElementById("sendto").selected;
     document.getElementById("logout").style.visibility = 'visible';
     
     socket.emit('rooms');
@@ -31,24 +33,87 @@ angular.module('chatApp').controller('RoomsController', function($scope, socket,
     socket.emit('users');
 	socket.on('userlist', function(usersList){
 		$scope.users = usersList;
+        /*for(var i = 0; i < usersList.length; i++) {
+            $scope.senders[usersList[i]] = {
+                nick: usersList[i],
+                msgs: []
+            }
+        }*/
+        //console.log($scope.senders);
 	});
         
-    socket.on('recv_privatemsg', function (sender, msg) {
+    socket.on('recv_privatemsg', function (sender, message) {
         $scope.msgs.push({
             sender: sender,
-            msg: msg
+            msg: message
         });
-        console.log("Message! " + $scope.msgs.sender + " " + $scope.msgs.msg);
-        sendTo = sender;
-        console.log($scope.msgs);
+        console.log("1: " + message);
+        
+        if(containsUser(sender)) {
+            $scope.senders[indexOfSender(sender)].msgs.push({
+            nick: sender,
+            msg: message
+        });
+        }
+        else {
+            $scope.senders.push({
+                nick: sender,
+                msgs: [{
+                    nick: sender,
+                    msg: message
+                }]
+            });
+            console.log("2: " + message);
+        }
+        console.log($scope.senders);
+        //console.log("Message! " + $scope.msgs.sender + " " + $scope.msgs.msg);
+        //sendTo = sender;
     });
     
-    $scope.send = function send() {
-        socket.emit('privatemsg', {nick: sendTo, message: $scope.newMsg});
-        $scope.msgs.push({sender: $scope.user, msg: $scope.newMsg});
+    $scope.send = function send(sendName) {
+        var message = document.getElementById(sendName).value;
+        console.log(message);
+        document.getElementById(sendName).value = "";
+        socket.emit('privatemsg', {nick: sendName, message: message});
+        $scope.msgs.push({sender: $scope.user, msg: message});
+        $scope.senders[indexOfSender(sendName)].msgs.push({
+            nick: $scope.user,
+            msg: message
+        });
+    };
+    
+    /*$scope.chooseUser = function chooseUser(user) {
+        sendTo = user;
+    };*/
+    
+    $scope.addSender = function addSender(sender) {
+        console.log("click damn it!! " + sender);
+        if(!containsUser(sender) && $scope.user !== sender) {
+            //console.log($scope.senders.indexOf(sender));
+            $scope.senders.push({
+                //id: $scope.senders.length,
+                nick: sender,
+                msgs: new Array()
+            });
+        }
+        console.log($scope.senders);
+    };
+    
+    function containsUser(str) {
+        for(var i = 0; i < $scope.senders.length; i++) {
+            if($scope.senders[i].nick === str) {
+                return true;
+            }
+        }
+        return false;
     }
     
-    $scope.chooseUser = function chooseUser(user) {
-        sendTo = user;
+    function indexOfSender(str) {
+        for(var i = 0; i < $scope.senders.length; i++) {
+            if($scope.senders[i].nick === str) {
+                return i;
+            }
+        }
+        return -1;
     }
 });
